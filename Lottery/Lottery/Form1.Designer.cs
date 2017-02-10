27050750fee6de51ce4a3e632daa2a27848573c7
Ian <ian@gmail.com>
2017-02-10 15:50:49 +0800

    UI設計 產生動態按鈕 亂數產生號碼

---
+++ Lottery/Lottery/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace Lottery
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tbBingoNumber = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnBingoAuto = new System.Windows.Forms.Button();
+            this.btnBingoReset = new System.Windows.Forms.Button();
+            this.btnBingoConfirm = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tabPage1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.BackColor = System.Drawing.Color.DarkGray;
+            this.tabPage3.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.tabPage3.Location = new System.Drawing.Point(4, 30);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(683, 506);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "今彩539";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.tabPage2.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.tabPage2.Location = new System.Drawing.Point(4, 30);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(683, 506);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "大樂透";
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.LightCoral;
+            this.tabPage1.Controls.Add(this.groupBox1);
+            this.tabPage1.Controls.Add(this.tbBingoNumber);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.btnBingoAuto);
+            this.tabPage1.Controls.Add(this.btnBingoReset);
+            this.tabPage1.Controls.Add(this.btnBingoConfirm);
+            this.tabPage1.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.tabPage1.Location = new System.Drawing.Point(4, 30);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(775, 506);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "BINGO BINGO賓果賓果";
+            // 
+            // tbBingoNumber
+            // 
+            this.tbBingoNumber.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.tbBingoNumber.Location = new System.Drawing.Point(42, 400);
+            this.tbBingoNumber.Name = "tbBingoNumber";
+            this.tbBingoNumber.Size = new System.Drawing.Size(392, 39);
+            this.tbBingoNumber.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(38, 373);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "選擇的號碼";
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 24;
+            this.listBox1.Location = new System.Drawing.Point(12, 28);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(221, 316);
+            this.listBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button2.Location = new System.Drawing.Point(12, 410);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 54);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "編輯";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // btnBingoAuto
+            // 
+            this.btnBingoAuto.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnBingoAuto.Location = new System.Drawing.Point(325, 445);
+            this.btnBingoAuto.Name = "btnBingoAuto";
+            this.btnBingoAuto.Size = new System.Drawing.Size(109, 54);
+            this.btnBingoAuto.TabIndex = 0;
+            this.btnBingoAuto.Text = "自動選號";
+            this.btnBingoAuto.UseVisualStyleBackColor = true;
+            this.btnBingoAuto.Click += new System.EventHandler(this.btnBingoAuto_Click);
+            // 
+            // btnBingoReset
+            // 
+            this.btnBingoReset.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnBingoReset.Location = new System.Drawing.Point(185, 445);
+            this.btnBingoReset.Name = "btnBingoReset";
+            this.btnBingoReset.Size = new System.Drawing.Size(109, 54);
+            this.btnBingoReset.TabIndex = 0;
+            this.btnBingoReset.Text = "重選";
+            this.btnBingoReset.UseVisualStyleBackColor = true;
+            this.btnBingoReset.Click += new System.EventHandler(this.btnBingoReset_Click);
+            // 
+            // btnBingoConfirm
+            // 
+            this.btnBingoConfirm.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnBingoConfirm.Location = new System.Drawing.Point(42, 445);
+            this.btnBingoConfirm.Name = "btnBingoConfirm";
+            this.btnBingoConfirm.Size = new System.Drawing.Size(109, 54);
+            this.btnBingoConfirm.TabIndex = 0;
+            this.btnBingoConfirm.Text = "儲存";
+            this.btnBingoConfirm.UseVisualStyleBackColor = true;
+            this.btnBingoConfirm.Click += new System.EventHandler(this.btnBingoConfirm_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.tabControl1.Location = new System.Drawing.Point(44, 35);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(783, 540);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Location = new System.Drawing.Point(530, 10);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(239, 493);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "歷史紀錄";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(12, 350);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 54);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "刪除";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button3.Location = new System.Drawing.Point(124, 350);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(109, 54);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "兌獎";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Khaki;
+            this.ClientSize = new System.Drawing.Size(866, 611);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TextBox tbBingoNumber;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnBingoReset;
+        private System.Windows.Forms.Button btnBingoConfirm;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.Button btnBingoAuto;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Lottery/Lottery/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lottery
+{
+    public partial class Form1 : Form
+    {
+        List<Button> myDButtonList1 = new List<Button>();
+        List<Button> myDButtonList2 = new List<Button>();
+        List<Button> myDButtonList3 = new List<Button>();
+        List<int> myNumberList1 = new List<int>();
+        List<int> myNumberList2 = new List<int>();
+        List<int> myNumberList3 = new List<int>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            generate_dButton(8, 10, tabPage1, myDButtonList1);
+            generate_dButton(7, 7, tabPage2, myDButtonList2);
+            generate_dButton(4, 10, tabPage3, myDButtonList3);
+
+        }
+
+        private void dButton_Click(object sender, EventArgs e)
+        {
+            Button myButton = (Button)sender;
+            myButton.BackColor = Color.Pink;
+            //MessageBox.Show(string.Format("{0}", myButton.Name));
+        }
+
+        private void generate_dButton(int row, int col, TabPage pageNum, List<Button> myDButton)
+        {
+            int Num = 1;
+            myDButton = new List<Button>();
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    Button dButton = new Button();
+                    //dButton.BackgroundImage
+                    dButton.BackColor = Color.White;
+                    dButton.ForeColor = Color.Black;
+                    dButton.Location = new Point(15 + 44 * j, 15 + 44 * i);
+                    dButton.Size = new Size(40, 40);
+                    dButton.Text = Num.ToString();
+                    dButton.Name = "btn" + Num.ToString();
+                    dButton.Font = new Font("微軟正黑體", 14);
+                    dButton.Click += new EventHandler(dButton_Click);
+
+                    //Controls.Add(dButton);
+                    Num++;
+                    pageNum.Controls.Add(dButton);
+                    myDButton.Add(dButton);
+
+                }
+            }
+
+        }
+
+        private void btnBingoConfirm_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBingoReset_Click(object sender, EventArgs e)
+        {
+            tbBingoNumber.Text = "";
+        }
+
+        private void btnBingoAuto_Click(object sender, EventArgs e)
+        {
+            Random R = new Random();
+            tbBingoNumber.Text = "";
+
+            for (int i = 0; i < 10; i++)
+            {
+                int indexList = R.Next(1,81);
+                tbBingoNumber.Text += indexList.ToString() + " ";
+                myDButtonList1[indexList].BackColor = Color.Pink;
+                myNumberList1.Add(indexList);                
+            }
+
+            foreach (int myList in myNumberList1)
+            {
+                Console.WriteLine(myList);
+            }
+
+        }
+    }
+}
